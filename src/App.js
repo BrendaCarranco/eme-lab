74de8f8e6809925654957b5653584183fc083fd3
usergithub <breen.78@hotmail.com>
2020-07-27 15:41:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Building from './components/buildingPage/Building';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Building />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/buildingPage/Building.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import eme from '../../img/eme-lab.png';
+
+const Building = () => {
+    return (
+        <div className="container-lg">
+            <div className="row">
+                <div className="col">
+                    <img src={eme} alt="" className="img-fluid" />
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Building;
