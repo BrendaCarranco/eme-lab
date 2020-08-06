7f690addc9601f91529c658af08ca010e0c7cc7a
Manuelujano <aketzali20@gmail.com>
2020-08-06 15:55:51 -0500

    logincss

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,18 @@
 import React, { useState, useCallback } from 'react';
 import { firebase } from '../../firebase';
-
+import './login.css'
+import logoeme from '../../img/logoeme.png';
+import Modal from 'react-modal';
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [user, setUser] = useState('');
     const [name, setName] = useState('');
-
+    
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    
+    
     const handleLogin = (e) => {
         e.preventDefault();
         if (!email.trim()) {
@@ -77,28 +82,61 @@ const Login = () => {
 
 
     return (
-        <div className='container mt-5'>
-            <div>
-                <h1>Iniciar sesión</h1>
-                <form onSubmit={handleLogin}>
-                    <input placeholder='Correo' type='email' onChange={e => setEmail(e.target.value)} />
-                    <input placeholder='Contraseña' type='password' onChange={e => setPassword(e.target.value)} />
-                    <button className='ml-2' type='submit' >Ingresar</button>
+    <div className="container">
+        <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5 border border-secondary rounded-0">
+            <div className='cad-body'>
+            
+                <img src={logoeme} align="center" className="logo mx-auto d-block p-4"/>
+               
+                <form className="form-signin" onSubmit={handleLogin}>
+                    <div className="form-label-group pt-5">
+                    
+                    <input className="form-control rounded-0 mx-auto d-block" placeholder='Correo' type='email' onChange={e => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-label-group">
+                  
+                    <input className="form-control rounded-0 mx-auto d-block" placeholder='Contraseña' type='password' onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <button className='btn btn-m btn-dark btn-block text-uppercase rounded-0' type='submit' >Ingresar</button>
                 </form>
-            </div>
-            <div className='mt-5'>
-                <h1>Registrarse</h1>
+                <p className="login-wrapper-footer-text text-center pt-5">¿No tienes cuenta?
+                <a onClick={() => setModalIsOpen(true)} className="badge badge-light">Registrate aquí</a>
+                <Modal isOpen={modalIsOpen} className="card modalregistro">
+                <h1 className="card-title mx-auto d-block text-center">Registrarse</h1>
+                <div className="card-body">
                 <form onSubmit={handleRegister}>
-                    <input placeholder='Correo' type='email' onChange={e => setEmail(e.target.value)} />
-                    <input placeholder='Contraseña' type='password' onChange={e => setPassword(e.target.value)} />
-                    <input placeholder='Nombre' type='text' onChange={e => setName(e.target.value)} />
-                    <button className='ml-2'>Ingresar</button>
+                <div className="form-label-group">
+                <input className="form-control rounded-0 mx-auto d-block inputmodal" placeholder='Correo' type='email' onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div className="form-label-group">
+                <input className="form-control rounded-0 mx-auto d-block inputmodal" placeholder='Contraseña' type='password' onChange={e => setPassword(e.target.value)} />
+                </div>
+                <div className="form-label-group">
+                <input className="form-control rounded-0 mx-auto d-block inputmodal" placeholder='Nombre' type='text' onChange={e => setName(e.target.value)} />
+                </div>
+                <button className='btn btn-m btn-dark btn-block text-uppercase rounded-0 btnmodal'>Ingresar</button>
                 </form>
                 {
                     user !== '' ? (<div>Logueado</div>) : (<div>no logueado</div>)
                 }
+                <div>
+                    <button className='btn btn-m btn-dark btn-block text-uppercase rounded-0 btnmodal' onClick={() => setModalIsOpen(false)}>Cerrar</button>
+                </div>
+                </div>
+                </Modal>
+                </p>
+            </div>
+            </div>
             </div>
         </div>
+    </div>
+
+    
+    
+
+        
     );
 };
 
